fb19130d742c352c2308eb92992d130d62bebee5
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-11-13 15:12:26 +0900

    REFACTOR : Changed method names in ArticleServiceTest

--- src/test/java/com/glenn/myblog/service/ArticleServiceTest.java
+++ src/test/java/com/glenn/myblog/service/ArticleServiceTest.java
@@ -20,6 +20,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 public class ArticleServiceTest {
+    private static final String TEST_TITLE = "test article";
+    private static final String TEST_NAME = "tester";
+    private static final String TEST_CONTENT = "test content";
 
     @InjectMocks
     private ArticleService articleService;
@@ -29,9 +32,9 @@ public class ArticleServiceTest {
 
     private Article article = Article.builder()
             .id(1L)
-            .title("test article")
-            .authorName("tester")
-            .content("test content")
+            .title(TEST_TITLE)
+            .authorName(TEST_NAME)
+            .content(TEST_CONTENT)
             .build();
 
     @BeforeEach
@@ -41,7 +44,7 @@ public class ArticleServiceTest {
 
     @DisplayName("게시글 전체 조회")
     @Test
-    public void findAll() {
+    public void 게시글_조회() {
         List<Article> articles = Arrays.asList(article);
 
         when(articleRepository.findAllByOrderByIdDesc()).thenReturn(articles);
@@ -49,53 +52,56 @@ public class ArticleServiceTest {
         List<ArticleDto> articleDtos = articleService.findAll();
         ArticleDto articleDto = articleDtos.get(0);
 
-        assertThat(articleDto.getAuthorName()).isEqualTo("tester");
-        assertThat(articleDto.getContent()).isEqualTo("test content");
+        assertThat(articleDto.getAuthorName()).isEqualTo(TEST_NAME);
+        assertThat(articleDto.getContent()).isEqualTo(TEST_CONTENT);
+
         verify(articleRepository, times(1)).findAllByOrderByIdDesc();
     }
 
     @DisplayName("ID로 특정 게시글 조회")
     @Test
-    public void findById() {
+    public void 특정_게시글_조회_ID로() {
         when(articleRepository.findById(1L)).thenReturn(Optional.of(article));
 
         ArticleDto articleDto = articleService.findById(1L);
 
-        assertThat(articleDto.getAuthorName()).isEqualTo("tester");
-        assertThat(articleDto.getContent()).isEqualTo("test content");
+        assertThat(articleDto.getAuthorName()).isEqualTo(TEST_NAME);
+        assertThat(articleDto.getContent()).isEqualTo(TEST_CONTENT);
         verify(articleRepository, times(1)).findById(eq(1L));
     }
 
-    @DisplayName("없는 ID로 특정 게시글 조회 시도시 예외 발생")
+    @DisplayName("없는 ID로 게시글 조회시 예외 발생")
     @Test
-    public void findById_예외() {
+    public void 게시글_조회_실패_없는_ID_참조() {
         when(articleRepository.findById(300L)).thenReturn(Optional.empty());
 
         assertThatThrownBy(() -> {
             articleService.findById(300L);
         }).isInstanceOf(ArticleNotFoundException.class);
+
         verify(articleRepository, times(1)).findById(eq(300L));
     }
 
     @DisplayName("게시글 생성")
     @Test
-    public void save() {
+    public void 게시글_생성() {
         when(articleRepository.save(any())).thenReturn(article);
 
         ArticleDto articleRequestDto = ArticleDto.of(article);
         ArticleDto articleResponseDto = articleService.save(articleRequestDto);
 
-        assertThat(articleResponseDto.getAuthorName()).isEqualTo("tester");
+        assertThat(articleResponseDto.getAuthorName()).isEqualTo(TEST_NAME);
+
         verify(articleRepository, times(1)).save(any());
     }
 
     @DisplayName("게시글 수정")
     @Test
-    public void update() {
+    public void 게시글_수정() {
         ArticleDto requestDto = ArticleDto.builder()
                 .id(1L)
-                .title("test article")
-                .authorName("tester")
+                .title(TEST_TITLE)
+                .authorName(TEST_NAME)
                 .content("after")
                 .build();
 
@@ -104,12 +110,13 @@ public class ArticleServiceTest {
         ArticleDto responseDto = articleService.update(1L, requestDto);
 
         assertThat(responseDto.getContent()).isEqualTo("after");
+
         verify(articleRepository, times(1)).findById(eq(1L));
     }
 
     @DisplayName("게시글 삭제")
     @Test
-    public void delete() {
+    public void 게시글_삭제() {
         articleService.deleteById(1L);
 
         verify(articleRepository, times(1)).deleteById(eq(1L));
